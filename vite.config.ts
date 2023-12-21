3edbba31d03e025056d76dd4a859fdf99f5ca31e
Pere Pages <pere.pages@ocado.com>
2023-12-21 12:36:50 +0100

    fix: gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 
 const GITHUB_PAGES_DEFAULT_FOLDER = "/docs";
+const PRODUCTION_PATH = "/platform-game";
 
 export default defineConfig({
+  base: PRODUCTION_PATH,
   build: {
     outDir: `.${GITHUB_PAGES_DEFAULT_FOLDER}`,
   },
